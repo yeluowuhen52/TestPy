447594239fca9be1f2a03b4686562bd7e89f15af
Jacy <jtq@winning.com.cn>
2024-06-27 15:27:34 +0800

    appium 安装卸载app

--- AppiumDemo.py
+++ AppiumDemo.py
@@ -5,13 +5,25 @@ desired_caps = dict()
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '12'
 desired_caps['deviceName'] = '127.0.0.1:7555'
-#今日头条
-desired_caps['appPackage'] = 'com.ss.android.article.news'
-desired_caps['appActivity'] = 'com.ss.android.article.news.activity.MainActivity'
+# 今日头条
+# desired_caps['appPackage'] = 'com.ss.android.article.news'
+# desired_caps['appActivity'] = 'com.ss.android.article.news.activity.MainActivity'
 
 # desired_caps['appPackage'] = 'com.android.settings'
 # desired_caps['appActivity'] = 'com.android.settings.SubSettings'
-
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+# 启动今日头条
+print(driver.current_package + '---' + driver.current_activity)
+
+driver.start_activity("com.ss.android.article.news", "com.ss.android.article.news.activity.MainActivity")
+
+print(driver.current_package + '33333---' + driver.current_activity)
 time.sleep(5)
+driver.terminate_app("com.ss.android.article.news")
+
+print(driver.is_app_installed("com.ss.android.article.news"))
+
+driver.install_app("D:\头条搜索极速版.apk")
+
 driver.quit()
