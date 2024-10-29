575c0ada7125be0d94e9bbe9d94fb3e321f18844
Jacy <jtq@winning.com.cn>
2024-10-29 22:38:04 +0800

    new

--- WindowTest.py
+++ WindowTest.py
@@ -10,4 +10,8 @@ driver.maximize_window()
 print(driver.title)
 
 time.sleep(3)
-driver.quit()
+
+driver.find_element_by_xpath('//*[@id="kw"]').send_keys("蜡笔小新")
+
+driver.find_element_by_xpath('//*[@id="kw"]').click()
+# driver.quit()
